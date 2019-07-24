e4bc46bafcca640c4baf5a2433170f17ba353996
datagutten <datagutten@datagutten.net>
2019-07-24 10:48:01 +0200

    Fix typo and remove end tag

--- config_alert_sample.php
+++ config_alert_sample.php
@@ -1,9 +1,8 @@
 <?Php
-$config['recipients']=array('you@yourdomain.com'); //Array of mail addresses to receive alers
+$config['recipients']=array('you@yourdomain.com'); //Array of mail addresses to receive alerts
 $config['mail_from']='voucher@yourdomain.com';
 $config['smtp_server']='yoursmtp.local';
 $config['smtp_port']=25;
 $config['mail_subject']='Few voucher codes left';
 $config['mail_body']='There are %d voucher codes left';
 $config['limit']=200; //Send message when there is X voucher codes left
-?>
